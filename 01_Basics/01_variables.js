9377627e524a0e04c99059006c65ab769a5a8db5
Mr-Sanruki <mr.sunruki@gmail.com>
2025-08-07 13:18:57 +0000

    variabe and datatype

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123232 
+const accountName = "Ruki"
+let accountNo = "113342"
+let accountState;
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountNo);
+console.log(accountState);
+
+console.table([accountId, accountName, accountNo, accountState ])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+let username = "Ruki"            //String Type
+let number = 989898898         //Number Type
+let gmail = "Ruki@google.com"  //String Type
+let loginStatus = false        //Boolean Type
+
+
+// alert( 3 - 7 ) Not Gonna work We are in Node js not in browser
+
+// console.log( 3 + 6 ); it works 
+
+//Code readability should be high
+ 
+/*
+    Types are :-
+    1. String Type => ""
+    2. Number Type
+    3. Boolean Type => True/False
+    4. null => Standalone value (Shown as Object in output)
+    5. undefined => Undefined   (Shown as Undefined in output)
+    6. bigint
+*/
+
+console.table([username, number, gmail, loginStatus])
+
+console.table([typeof username,typeof number,typeof gmail,typeof loginStatus])
+
