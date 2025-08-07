f72342ad98d71ba27001d342262294c0df59971f
Mr-Sanruki <mr.sunruki@gmail.com>
2025-08-07 18:52:06 +0000

    Datatype conversion

---
+++ 01_Basics/03_Datatype_Conversion.js
@@ -0,0 +1,125 @@
+// // conversion of number into string , boolean , null , undefined
+
+// let age = 33 //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+// // conversion of string into number , boolean , null , undefined
+
+// let age = "33" //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+// // conversion of non string into number , boolean , null , undefined
+
+// let age = "33abc" //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+// // conversion of null into number , boolean , null , undefined
+
+// let age = null //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+// // conversion of undefined into number , boolean , null , undefined
+
+// let age ; //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+// // conversion of True boolean into number , boolean , null , undefined
+
+// let age = true //initial value
+
+// let number_age = Number(age)       //conversion into number 
+// let string_age = String(age)       //conversion into string
+// let boolean_age = Boolean(age)     //conversion into boolean         
+
+// console.log("age: " + age)
+// console.log("Type: " + typeof age)
+
+// console.log("Number age: " + number_age)
+// console.log("Type: " + typeof number_age)
+
+// console.log("String age: " + string_age)
+// console.log("Type: " +typeof string_age)
+
+// console.log("Boolean age: " + boolean_age)
+// console.log("Type: " +typeof boolean_age)
+
+
+
+
+
+
